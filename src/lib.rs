345d4ea4c1a339ee8d8097f99878a0817eee6d75
Sam Elliott <ashe2@cs.washington.edu>
2016-03-12 00:39:15 -0800

    So much parameterisation!

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
 
-use std::ops::*;
+use std::fmt;
+use std::ops::{Add, Div, Mul, Neg, Sub};
+
+extern crate num;
+use num::{Num, Float, Zero, One};
 
 #[cfg(test)]
 use std::f32::consts;
@@ -7,89 +11,92 @@ use std::f32::consts;
 use std::f32;
 
 /// Dual Numbers
-#[derive(Copy, Clone, PartialEq, Debug)]
-pub struct DualNumber {
-    real: f32,
-    dual: f32,
+#[derive(Copy, Clone, PartialEq, Hash, Debug)]
+pub struct Dual<F> {
+    pub real: F,
+    pub dual: F,
 }
 
-impl DualNumber {
-    pub fn new(real: f32, deriv: f32) -> DualNumber {
-        DualNumber {
+pub type Dual32 = Dual<f32>;
+pub type Dual64 = Dual<f64>;
+
+impl<F: Copy + Num> Dual<F> {
+    pub fn new(real: F, deriv: F) -> Dual<F> {
+        Dual {
             real: real,
             dual: deriv,
         }
     }
 
-    pub fn real(self) -> f32 {
-        self.real
-    }
-
-    pub fn derivative(self) -> f32 {
+    pub fn derivative(self) -> F {
         self.dual
     }
 }
 
-impl Default for DualNumber {
-    fn default() -> DualNumber {
-        DualNumber::new(0.0, 0.0)
+impl<F: Copy + Num> Zero for Dual<F> {
+    fn zero() -> Dual<F> {
+        Dual::new(F::zero(), F::zero())
+    }
+
+    fn is_zero(&self) -> bool {
+        self.real.is_zero() && self.dual.is_zero()
     }
 }
 
-impl From<f32> for DualNumber {
-    fn from(n: f32) -> DualNumber {
-        DualNumber::new(n, 0.0)
+impl<F: Copy + Num> One for Dual<F> {
+    fn one() -> Dual<F> {
+        Dual::new(One::one(), One::one())
     }
 }
 
-impl Add for DualNumber {
-    type Output = DualNumber;
+impl<F: Copy + Num> Add<Dual<F>> for Dual<F> {
+    type Output = Dual<F>;
 
-    fn add(self, other: DualNumber) -> DualNumber {
-        DualNumber::new(self.real + other.real, self.dual + other.dual)
+    fn add(self, other: Dual<F>) -> Dual<F> {
+        Dual::new(self.real + other.real, self.dual + other.dual)
     }
 }
 
 #[test]
 fn test_add_struct() {
-    let x = DualNumber::new(3.0, 4.0);
-    let y = DualNumber::new(2.0, 3.0);
+    let x = Dual::new(3.0, 4.0);
+    let y = Dual::new(2.0, 3.0);
 
     let z = x + y;
     assert!(z.real == 5.0);
     assert!(z.dual == 7.0);
 }
 
-impl Sub for DualNumber {
-    type Output = DualNumber;
+impl<F: Copy + Num> Sub<Dual<F>> for Dual<F> {
+    type Output = Dual<F>;
 
-    fn sub(self, other: DualNumber) -> DualNumber {
-        DualNumber::new(self.real - other.real, self.dual - other.dual)
+    fn sub(self, other: Dual<F>) -> Dual<F> {
+        Dual::new(self.real - other.real, self.dual - other.dual)
     }
 }
 
 #[test]
 fn test_sub() {
-    let x = DualNumber::new(3.0, 4.0);
-    let y = DualNumber::new(2.0, 12.0);
+    let x = Dual::new(3.0, 4.0);
+    let y = Dual::new(2.0, 12.0);
 
     let z = x - y;
     assert!(z.real == 1.0);
     assert!(z.dual == -8.0);
 }
 
-impl Neg for DualNumber {
-    type Output = DualNumber;
+impl<F: Copy + Num + Neg<Output = F>> Neg for Dual<F> {
+    type Output = Dual<F>;
 
-    fn neg(self) -> DualNumber {
-        DualNumber::new(-self.real, -self.dual)
+    fn neg(self) -> Dual<F> {
+        Dual::new(-self.real, -self.dual)
     }
 }
 
 #[test]
 fn test_neg_plus() {
-    let x = DualNumber::new(3.0, 4.0);
-    let y = DualNumber::new(2.0, 12.0);
+    let x = Dual::new(3.0, 4.0);
+    let y = Dual::new(2.0, 12.0);
 
     let z1 = x - y;
     let z2 = x + (-y);
@@ -98,82 +105,110 @@ fn test_neg_plus() {
 
 #[test]
 fn test_neg_zero() {
-    let x = DualNumber::new(3.0, 4.0);
-    let zero: DualNumber = Default::default();
+    let x = Dual::new(3.0, 4.0);
+    let zero: Dual32 = Zero::zero();
 
     let z1 = -x;
     let z2 = zero - x;
     assert!(z1 == z2);
 }
 
-impl Mul for DualNumber {
-    type Output = DualNumber;
+impl<F: Copy + Num> Mul<Dual<F>> for Dual<F> {
+    type Output = Dual<F>;
 
-    fn mul(self, other: DualNumber) -> DualNumber {
-        DualNumber::new(self.real * other.real,
-                        (self.real * other.dual) + (self.dual * other.real))
+    fn mul(self, other: Dual<F>) -> Dual<F> {
+        Dual::new(self.real * other.real,
+                  (self.real * other.dual) + (self.dual * other.real))
     }
 }
 
 #[test]
 fn test_mul() {
-    let x = DualNumber::new(3.0, 4.0);
-    let y = DualNumber::new(1.0, 2.0);
+    let x = Dual::new(3.0, 4.0);
+    let y = Dual::new(1.0, 2.0);
 
     let z = x * y;
     assert!(z.real == 3.0);
     assert!(z.dual == 10.0);
 }
 
-impl Div for DualNumber {
-    type Output = DualNumber;
+impl<F: Copy + Num> Div<Dual<F>> for Dual<F> {
+    type Output = Dual<F>;
 
-    fn div(self, other: DualNumber) -> DualNumber {
-        DualNumber::new(self.real / other.real,
-                        ((self.dual * other.real) - (self.real * other.dual)) /
-                        (other.real * other.real))
+    fn div(self, other: Dual<F>) -> Dual<F> {
+        Dual::new(self.real / other.real,
+                  ((self.dual * other.real) - (self.real * other.dual)) / (other.real * other.real))
     }
 }
 
 #[test]
 fn test_div() {
-    let x = DualNumber::new(3.0, 4.0);
-    let y = DualNumber::new(1.0, 2.0);
+    let x = Dual::new(3.0, 4.0);
+    let y = Dual::new(1.0, 2.0);
 
     let z = x / y;
     assert!(z.real == 3.0);
     assert!(z.dual == -2.0);
 }
 
-impl DualNumber {
-    pub fn sin(self) -> DualNumber {
-        DualNumber::new(self.real.sin(), self.dual * self.real.cos())
+impl<F: Copy + Float> Dual<F> {
+    pub fn sin(self) -> Dual<F> {
+        Dual::new(self.real.sin(), self.dual * self.real.cos())
     }
 
-    pub fn cos(self) -> DualNumber {
-        DualNumber::new(self.real.cos(), self.dual * self.real.sin())
+    pub fn cos(self) -> Dual<F> {
+        Dual::new(self.real.cos(), (-F::one()) * self.dual * self.real.sin())
     }
 
-    pub fn exp(self) -> DualNumber {
-        DualNumber::new(self.real.exp(), self.dual * self.real.exp())
+    pub fn tan(self) -> Dual<F> {
+        self.sin() / self.cos()
     }
 
-    pub fn ln(self) -> DualNumber {
-        DualNumber::new(self.real.ln(), self.dual / self.real)
+    pub fn exp(self) -> Dual<F> {
+        Dual::new(self.real.exp(), self.dual * self.real.exp())
     }
 
-    pub fn sqrt(self) -> DualNumber {
-        DualNumber::new(self.real.sqrt(), self.dual / (2.0 * self.real.sqrt()))
+    pub fn ln(self) -> Dual<F> {
+        Dual::new(self.real.ln(), self.dual / self.real)
     }
+
+    pub fn sqrt(self) -> Dual<F> {
+        let two = F::one() + F::one();
+        Dual::new(self.real.sqrt(),
+                  self.dual / (two * self.real.sqrt()))
+    }
+}
+
+impl<F: Copy + Num> From<F> for Dual<F> {
+    fn from(real: F) -> Dual<F> {
+        Dual::new(real, F::one())
+    }
+}
+
+impl<F> fmt::Display for Dual<F>
+    where F: fmt::Display + Num + PartialOrd + Copy
+{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        if self.real < Zero::zero() {
+            write!(f, "{}-{}ε", self.real, F::zero() - self.dual)
+        } else {
+            write!(f, "{}+{}ε", self.real, self.dual)
+        }
+    }
+}
+
+#[cfg(test)]
+fn diff(x : f32, y : f32) -> f32 {
+    (x - y).abs()
 }
 
 #[test]
 fn test_sin() {
-    let x: DualNumber = From::from(consts::PI);
+    let x: Dual32 = From::from(0.0);
     let y = x.sin();
 
-    let real_diff = y.real - 0.0;
-    let dual_diff = y.dual - 1.0;
+    let real_diff = diff(y.real, 0.0);
+    let dual_diff = diff(y.dual, 1.0);
 
     assert!(real_diff < f32::EPSILON);
     assert!(dual_diff < f32::EPSILON);
@@ -181,11 +216,11 @@ fn test_sin() {
 
 #[test]
 fn test_cos() {
-    let x: DualNumber = From::from(consts::PI);
+    let x: Dual32 = From::from(consts::PI);
     let y = x.cos();
 
-    let real_diff = y.real - 1.0;
-    let dual_diff = y.dual - 0.0;
+    let real_diff = diff(y.real, -1.0);
+    let dual_diff = diff(y.dual, 0.0);
 
     assert!(real_diff < f32::EPSILON);
     assert!(dual_diff < f32::EPSILON);
@@ -193,11 +228,11 @@ fn test_cos() {
 
 #[test]
 fn test_exp() {
-    let x: DualNumber = From::from(1.0);
+    let x: Dual32 = From::from(1.0);
     let y = x.exp();
 
-    let real_diff = y.real - consts::E;
-    let dual_diff = y.dual - consts::E;
+    let real_diff = diff(y.real, consts::E);
+    let dual_diff = diff(y.dual, consts::E);
 
     assert!(real_diff < f32::EPSILON);
     assert!(dual_diff < f32::EPSILON);
@@ -205,11 +240,11 @@ fn test_exp() {
 
 #[test]
 fn test_ln() {
-    let x: DualNumber = From::from(consts::E);
+    let x: Dual32 = From::from(1.0);
     let y = x.ln();
 
-    let real_diff = y.real - 1.0;
-    let dual_diff = y.dual - consts::E;
+    let real_diff = diff(y.real, 0.0);
+    let dual_diff = diff(y.dual, 1.0);
 
     assert!(real_diff < f32::EPSILON);
     assert!(dual_diff < f32::EPSILON);
@@ -217,11 +252,13 @@ fn test_ln() {
 
 #[test]
 fn test_sqrt() {
-    let x: DualNumber = From::from(100.0);
-    let y = x.ln();
+    let x: Dual32 = From::from(4.0);
+    let y = x.sqrt();
 
-    let real_diff = y.real - 10.0;
-    let dual_diff = y.dual - 0.05;
+    println!("{}", y);
+
+    let real_diff = diff(y.real, 2.0);
+    let dual_diff = diff(y.dual, 0.25);
 
     assert!(real_diff < f32::EPSILON);
     assert!(dual_diff < f32::EPSILON);
